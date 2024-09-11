e32f32e4e46d25e5f914dbd396ba65eb1f39f2b0
turingdance <271151388@qq.com>
2024-09-11 20:44:02 +0800

    A

--- traefikjwtauthor.go
+++ traefikjwtauthor.go
@@ -3,6 +3,7 @@ package traefikjwtauthor
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -15,21 +16,23 @@ const (
 	DefaultSecret string = "turingdance@1232w3e4r"
 )
 
-// Config the plugin configuration.
+// 系统会将这些东西传递到第三方
 type Config struct {
-	Param     string   `json:"param,omitempty" toml:"param,omitempty" yaml:"param,omitempty"`
-	Secrect   string   `json:"secret,omitempty" toml:"secret,omitempty" yaml:"secret,omitempty"`
-	Prefix    string   `json:"prefix,omitempty" toml:"prefix,omitempty" yaml:"prefix,omitempty"`
-	WhiteList []string `json:"whiteList,omitempty" toml:"whiteList,omitempty" yaml:"whiteList,omitempty"`
+	Param        string   `json:"param,omitempty" toml:"param,omitempty" yaml:"param,omitempty"`
+	Secrect      string   `json:"secret,omitempty" toml:"secret,omitempty" yaml:"secret,omitempty"`
+	TokenPrefix  string   `json:"tokenPrefix,omitempty" toml:"tokenPrefix,omitempty" yaml:"tokenPrefix,omitempty"`
+	HeaderPrefix string   `json:"headerPrefix,omitempty" toml:"headerPrefix,omitempty" yaml:"headerPrefix,omitempty"`
+	WhiteList    []string `json:"whiteList,omitempty" toml:"whiteList,omitempty" yaml:"whiteList,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{
-		WhiteList: []string{},
-		Secrect:   DefaultSecret,
-		Param:     TokenParam,
-		Prefix:    TokenPrefix,
+		WhiteList:    []string{},
+		Secrect:      DefaultSecret,
+		Param:        TokenParam,
+		HeaderPrefix: "X-Turing-",
+		TokenPrefix:  TokenPrefix,
 	}
 }
 
@@ -63,10 +66,24 @@ func (r *JwtAuthor) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	// 如果没有过,那么直接采用鉴权策略
 	if !pass {
 		tokenstr := req.Header.Get(r.config.Param)
-		tokenstr = strings.TrimPrefix(tokenstr, r.config.Prefix)
-		_, err := r.tokenmgr.ParseToken(tokenstr)
+		tokenstr = strings.TrimPrefix(tokenstr, r.config.TokenPrefix)
+		tokenMap, err := r.tokenmgr.ParseToken(tokenstr)
 		if err != nil {
 			pass = false
+		}
+		for key, value := range tokenMap {
+			switch value := value.(type) {
+			case int64:
+			case int32:
+			case int:
+				req.Header.Add(key, fmt.Sprintf("%s-%d", r.config.HeaderPrefix, value))
+			case float32:
+			case float64:
+				req.Header.Add(key, fmt.Sprintf("%s-%f", r.config.HeaderPrefix, value))
+			case string:
+				req.Header.Add(key, fmt.Sprintf("%s-%s", r.config.HeaderPrefix, value))
+			}
+
 		}
 	}
 	if pass {
